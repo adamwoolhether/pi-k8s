6b5e7d369b60f194900f05323beb8ae672491a42
adamwoolhether <adamwoolhether@gmail.com>
2020-12-06 07:23:39 +0000

    down

--- nodesdown.sh
+++ nodesdown.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+#Power off entire cluster
 for i in {1..3}; do ssh -o ConnectTimeout=5 node$i 'sudo poweroff'; done
 echo "all nodes powered off"
 for ((i=5; i>1; i--)); do echo "shutown in $i" && sleep 1; done
 echo "goodbye"
-#sudo poweroff
+sudo poweroff
 exit
